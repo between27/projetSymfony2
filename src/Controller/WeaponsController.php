4923e9d2a675f3d1d626004a8bc97267a77de25f
Aboubacar <djarimias@gmail.com>
2024-01-31 15:52:36 +0100

    Ajout du formulaire de création et de suppression d'une arme seulement faisable par l'admin

--- src/Controller/WeaponsController.php
+++ src/Controller/WeaponsController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\WeaponPhoto;
+use App\Entity\Weapons;
+use App\Form\WeaponsType;
 use App\Repository\WeaponsRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,9 +19,6 @@ class WeaponsController extends AbstractController
     public function index(WeaponsRepository $weapon): Response
     {
         $weapons = $weapon->findAll();
-
-
-
         return $this->render('weapons/index.html.twig', [
             'controller_name' => 'WeaponsController',
             'weapons' => $weapons
@@ -30,11 +33,55 @@ class WeaponsController extends AbstractController
         if (!$weapon) {
             throw $this->createNotFoundException('L\'arme demandée n\'existe pas.');
         }
-
         return $this->render('weapons/show.html.twig', [
             'controller_name' => 'WeaponsController',
             'weapon' => $weapon
         ]);
     }
+
+    #[isGranted('ROLE_ADMIN')]
+    #[Route('/weapons/create', name: 'app_weapons_create')]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $weapon = new Weapons();
+        $form = $this->createForm(WeaponsType::class, $weapon);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $photoUrl = $form->get('photoUrl')->getData();
+            $weaponPhoto = new WeaponPhoto();
+            $weaponPhoto->setUrl($photoUrl);
+            $entityManager->persist($weaponPhoto);
+
+            $weapon->setPhotoUrl($weaponPhoto);
+            $entityManager->persist($weapon);
+            $entityManager->flush();
+            $this->addFlash('success', 'Arme ajoutée avec succès !');
+            return $this->redirectToRoute('app_weapons');
+        }
+
+        return $this->render('weapons/create.html.twig', [
+            'controller_name' => 'WeaponsController',
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    #[isGranted('ROLE_ADMIN')]
+    #[Route('/weapon/delete/{id}', name: 'weapon_delete')]
+    public function delete(Request $request, EntityManagerInterface $entityManager, int $id): Response
+    {
+        $weapon = $entityManager->getRepository(Weapons::class)->find($id);
+
+        if (!$weapon) {
+            throw $this->createNotFoundException('Aucune arme trouvée pour l\'id ' . $id);
+        }
+
+        $entityManager->remove($weapon);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_weapons');
+    }
+
 
 }
